11b458ef2da57b6ce1c96f22d48b1394ff26c53a
shubham9991 <shubham321098@gmail.com>
2024-03-17 05:55:34 -0400

    Faq Questions, and Services page structure ready

---
+++ Services.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('open');
+});
+
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('open');
+});
