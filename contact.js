98f607c70e822bc4257edc8a7eff3b886183994f
shubham9991 <shubham321098@gmail.com>
2024-07-20 23:37:14 +0530

    Deleted unnecessary files and folders

--- contact.js
+++ contact.js
@@ -1,3 +1,28 @@
+let lastScrollY = window.scrollY;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        navbar.classList.add('hidden');
+    } else {
+        // Scrolling up
+        navbar.classList.remove('hidden');
+    }
+    lastScrollY = window.scrollY;
+});
+
+function validatePhoneNumber(input) {
+    // Remove any non-numeric characters
+    input.value = input.value.replace(/\D/g, '');
+
+    // Limit the input to 10 digits
+    if (input.value.length > 13) {
+        input.value = input.value.slice(0, 13);
+    }
+}
+
+// GSAP initialization and animation logic
 function initGSAP() {
     var menu = document.querySelector("#nav i");
     var cross = document.querySelector("#full i");
